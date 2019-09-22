6e976eb75a32daf5b1f35cadff33e91c2fa60cd6
Pizleyos <55303056+Pizleyos@users.noreply.github.com>
2019-09-23 01:35:46 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -71,5 +71,166 @@ public class Main {
 //            System.out.println(linia);
 //        }
 
+//////////Pętle - while i do - while
+
+        //1.
+        //Wewnątrz pętli while (np. 10 razy) pobieraj produkt do kupienia i wyświetlaj go na ekran
+        //w postaci : „Dodałem do koszyka <nazwa>, to nasz <numer iteracji> produkt!”
+
+//        Scanner scanner = new Scanner(System.in);
+//        int i = 1;
+//
+//        while (i < 11) {
+//            System.out.println("Podaj nazwę produktu:");
+//            String produkt = scanner.nextLine();
+//            System.out.println("Dodałem do koszyka " + produkt + ", to nasz " + i + ". produkt");
+//            i++;
+//        }
+
+        //2.
+        //Stwórz pętle while , która wykona 5 iteracji. Wewnątrz pętli pobieraj z konsoli dowolną
+        //wartość typu int . Po wyjściu z pętli zwróć sumę tych wartości.
+
+//        Scanner scanner = new Scanner(System.in);
+//        int i = 1;
+//        int suma = 0;
+//        while (i < 6) {
+//            System.out.println("Podaj " + i + ". liczbę:");
+//            int skladnik = scanner.nextInt();
+//            suma = suma + skladnik;
+//            i++;
+//        }
+//        System.out.println("Suma podanych liczb to:" + suma);
+
+        //3.
+        //Jak wyżej, ale zwróć sumę tylko tych wartości, które były większe od 10;
+
+//        Scanner scanner = new Scanner(System.in);
+//        int i = 1;
+//        int suma = 0;
+//        while (i < 6) {
+//            System.out.println("Podaj " + i + ". liczbę:");
+//            int skladnik = scanner.nextInt();
+//            if (skladnik > 10) {
+//                suma = suma + skladnik;
+//            } else {
+//                System.out.println("Podana liczba nie jest większa od 10.");
+//            }
+//            i++;
+//        }
+//        System.out.println("Suma podanych liczb większych od 10 to:" + suma);
+
+        //4.
+        //4.*Pobieraj i wyświetlaj dowolny ciąg znaków od użytkownika tak długo, aż nie napisze „koniec"
+
+//        Scanner scanner = new Scanner(System.in);
+//        String produkt;
+//
+//        do {
+//            System.out.println("Podaj nazwę produktu:");
+//            produkt = scanner.nextLine();
+//            System.out.println(produkt);
+//        } while (!produkt.equals("koniec"));
+
+        //5.
+        //5.*Jak wyżej, z i bez wyświetlania słowa „koniec"
+
+        // ^ Tutaj bez, w poprzednim z.
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String produkt;
+//        do {
+//            System.out.println("Podaj nazwę produktu:");
+//            produkt = scanner.nextLine();
+//            if (!produkt.equals("koniec")) {
+//                System.out.println(produkt);
+//            }
+//        } while (!produkt.equals("koniec"));
+
+////////// Tablice jednowymiarowe - petle for each
+
+//        int[] tab = new int[10];
+//        System.out.println("Tablica tab ma długość : " + tab.length);
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("Wartość w tablicy przed wpisaniem tab[" + i + "] : " + tab[i]);
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            tab[i] = i + 50;
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("Wartość w tablicy po wpisaniu tab[" + i + "] : " + tab[i]);
+//        }
+
+        //1.
+        //Wewnątrz pętli while (np. 10 razy) pobieraj produkt do kupienia i wprowadzaj go do
+        //utworzonej tablicy typu String. Wyświetl wszystkie produkty z wykorzystaniem pętli for
+        //oraz for each
+
+//        Scanner scanner = new Scanner(System.in);
+//        String[] produkty = new String[10];
+//        int i = 0;
+//
+//        while (i < 10) {
+//            System.out.println("Podaj nazwę produktu:");
+//            String produkt = scanner.nextLine();
+//            System.out.println("Dodałem do koszyka " + produkt + ", to nasz " + (i + 1) + ". produkt");
+//            produkty[i] = produkt;
+//            i++;
+//        }
+//        System.out.println("Lista wprowadzonych produktów (for):");
+//
+//        for (int x = 0; x < 10; x++) {
+//            System.out.println("  - " + produkty[x]);
+//        }
+//        System.out.println("Lista wprowadzonych produktów (foreach):");
+//        for (String produkt : produkty) {
+//            System.out.println("  - " + produkt);
+//        }
+
+
+        //2.
+        //Utwórz tablicę przechowującą wartości typu int o rozmiarze zadanym z konsoli. Wypełnij
+        //ją wartościami wewnątrz pętli for. Zwróć sumę tych wartości.
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Podaj ilość składników w tabeli:");
+//        int dlugoscTabeli = scanner.nextInt();
+//        int[] tablicaWartosci = new int[dlugoscTabeli];
+//        int sumaSkladnikow = 0;
+//        int i = 1;
+//
+//        for (int skladnik : tablicaWartosci) {
+//            System.out.println("Podaj " + i + ". składnik:");
+//            skladnik = scanner.nextInt();
+//            System.out.println(skladnik);
+//            sumaSkladnikow = sumaSkladnikow + skladnik;
+//            i++;
+//        }
+//        System.out.println("Suma wprowadzonych skłądników wynosi: " + sumaSkladnikow);
+
+
+        //3.
+        //Stwórz tablicę zawierającą 5 imion. Wewnątrz pętli for each dopisuj imiona do zmiennej
+        //typu String, oddzielając je przecinkami. Wyświetl utworzony łańcuch znaków. Np.
+        //„Małgorzata, Jan,
+
+        Scanner scanner = new Scanner(System.in);
+        String[] imiona = new String[5];
+
+        for (int i=0; i<5; i++) {
+            imiona[i] = (scanner.nextLine() + ",");
+        }
+        for (String imie : imiona) {
+            System.out.print(imie);
+        }
+
+
+
+        //4.
+        //4.*Jak wyżej, ale dopisuj tylko imiona, które składają się z mniej, niż 5 znaków.
+
+
     }
 }
